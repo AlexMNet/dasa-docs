3f0c8d4c4f9229cebc404403b5dee1a9e894e65d
Alex Maldonado <alex@Alexs-MBP-2021.home>
2022-08-17 19:17:24 -0700

    update docusaurus config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,46 +1,46 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
-require("dotenv").config();
-const lightCodeTheme = require("prism-react-renderer/themes/github");
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+require('dotenv').config();
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "DASA Docs",
-  tagline: "Data Structures and Algorithms",
-  url: process.env.SITE_URL,
-  baseUrl: process.env.BASE_URL,
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
-  favicon: "img/dasa-docs-favicon.ico",
+  title: 'DASA Docs',
+  tagline: 'Data Structures and Algorithms',
+  url: 'https://alexmnet.github.io',
+  baseUrl: '/dasa-docs/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/dasa-docs-favicon.ico',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: process.env.ORG_NAME, // Usually your GitHub org/user name.
-  projectName: "dasa-docs", // Usually your repo name.
-  deploymentBranch: "gh-pages",
+  organizationName: 'alexmnet', // Usually your GitHub org/user name.
+  projectName: 'dasa-docs', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
   trailingSlash: true,
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: "en",
-    locales: ["en"],
+    defaultLocale: 'en',
+    locales: ['en'],
   },
 
   presets: [
     [
-      "classic",
+      'classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          routeBasePath: "/",
-          sidebarPath: require.resolve("./sidebars.js"),
+          routeBasePath: '/',
+          sidebarPath: require.resolve('./sidebars.js'),
         },
 
         theme: {
-          customCss: require.resolve("./src/css/custom.css"),
+          customCss: require.resolve('./src/css/custom.css'),
         },
       }),
     ],
@@ -50,46 +50,46 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: "DASA Docs",
+        title: 'DASA Docs',
         logo: {
-          alt: "Dasa Docs Logo",
-          src: "img/dasa-docs-favicon.ico",
-          srcDark: "img/dasa-docs-favicon.ico",
+          alt: 'Dasa Docs Logo',
+          src: 'img/dasa-docs-favicon.ico',
+          srcDark: 'img/dasa-docs-favicon.ico',
         },
         items: [
           {
-            href: "https://github.com/AlexMNet/dasa-docs",
-            label: "GitHub",
-            position: "right",
+            href: 'https://github.com/AlexMNet/dasa-docs',
+            label: 'GitHub',
+            position: 'right',
           },
         ],
       },
       footer: {
-        style: "dark",
+        style: 'dark',
         links: [
           {
-            title: "Docs",
+            title: 'Docs',
             items: [
               {
-                label: "About",
-                to: "/",
+                label: 'About',
+                to: '/',
               },
             ],
           },
           {
-            title: "Community",
+            title: 'Community',
             items: [
               {
-                label: "Personal Site",
-                href: "https://www.alexmaldonado.dev/",
+                label: 'Personal Site',
+                href: 'https://www.alexmaldonado.dev/',
               },
               {
-                label: "LinkedIn",
-                href: "https://www.linkedin.com/in/alexmnet/",
+                label: 'LinkedIn',
+                href: 'https://www.linkedin.com/in/alexmnet/',
               },
               {
-                label: "Github",
-                href: "https://github.com/AlexMNet",
+                label: 'Github',
+                href: 'https://github.com/AlexMNet',
               },
             ],
           },
@@ -101,10 +101,10 @@ const config = {
         darkTheme: darkCodeTheme,
       },
       liveCodeBlock: {
-        playgroundPosition: "bottom",
+        playgroundPosition: 'bottom',
       },
     }),
-  plugins: ["@docusaurus/theme-live-codeblock"],
+  plugins: ['@docusaurus/theme-live-codeblock'],
 };
 
 module.exports = config;
